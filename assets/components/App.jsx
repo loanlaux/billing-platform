3886965619baadf1a27d660243022a24f4325457
Loan Laux <contact@loanlaux.fr>
2018-04-08 12:30:33 +0200

    Add sidebar menu

--- assets/components/App.jsx
+++ assets/components/App.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import AppBar from "material-ui/AppBar";
+import Drawer from "material-ui/Drawer";
 
 import NotFound from "./NotFound";
 import InvoiceListContainer from "../containers/InvoiceListContainer";
 
-const App = () => (
-  <div>
-    <AppBar title="Invoices" />
-    <main>
-      <Switch>
-        <Route path="/" component={InvoiceListContainer} />
-        <Route path="/*" component={NotFound} />
-      </Switch>
-    </main>
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          onLeftIconButtonClick={(showMenu) => this.setState({ showMenu })}
+          style={{ position: "fixed" }}
+          title="Invoices"
+        />
+        <Drawer
+          docked={false}
+          open={this.state.showMenu}
+          onRequestChange={(showMenu) => this.setState({ showMenu })}
+        />
+        <main>
+          <Switch>
+            <Route path="/" component={InvoiceListContainer} />
+            <Route path="/*" component={NotFound} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
